7b9ab5f1b0572a74f239cf005e7f0de2d36da327
Alevax <alexandrerediffusion@gmail.com>
2026-03-27 11:52:09 +0100

    Update config.php

--- public/config.php
+++ public/config.php
@@ -1,15 +1,15 @@
 <?php
-$host = 'https://uldldhzwsijrfvwbdjvv.supabase.co';
+$host = 'uldldhzwsijrfvwbdjvv.supabase.co'; 
 $port = '5432';
-$db   = 'Noxa';
-$user = 'ton_user';
-$pass = 'ton_motdepasse';
+$db   = 'Noxa';        
+$user = 'postgres';     
+$pass = 'Jx@pHGLws?NmB&8';  
+
 $dsn = "pgsql:host=$host;port=$port;dbname=$db;";
 
 try {
     $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    echo "Connexion réussie !";
+
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
-?>
